63bbecb7ab36016846dd4090a4cbe258d0b1efb6
Ihar Ivanchykau <v-iivanchykau@ebsco.com>
2020-07-17 21:38:33 +0300

    initial commit

---
+++ precache-manifest.0cfd2ee47e22cc97d13cb541dbe54bf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadb77adc1697fc14b0c439755f8f0a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c0616da1ee2cbba9996",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca294ce92cbb2988000a",
+    "url": "/static/js/2.2aef1092.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2aef1092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0616da1ee2cbba9996",
+    "url": "/static/js/main.a2c0ce6b.chunk.js"
+  },
+  {
+    "revision": "9d4efc016744dc63873f",
+    "url": "/static/js/runtime-main.9663fd2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
